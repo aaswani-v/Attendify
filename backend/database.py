5980f57247494f2d517618d0f61c8fbebc7f5d52
Abhishek-yadav04 <yadavabhishekrampal@gmail.com>
2026-01-16 15:17:52 +0530

    refactor: remove unused database and services modules

--- backend/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
-# Database setup
-engine = create_engine('sqlite:///attendance.db')
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-class Base(DeclarativeBase):
-    pass
